08c3f331999b3467cdfd9d42bfb50482feb557e5
Luke Kysow <lkysow@gmail.com>
2017-09-12 22:50:57 -0700

    Print version too

--- README.md
+++ README.md
@@ -11,11 +11,14 @@ import (
 	"log"
 )
 
+var version = "1"
+
 func main() {
 	hostname, _ := os.Hostname()
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, from %s! Our config is %q and our secret is %q",
+		fmt.Fprintf(w, "Hello, from %s! My version is %s, our config is %q and our secret is %q",
 			hostname,
+			version,
 			os.Getenv("CONFIG"),
 			os.Getenv("SECRET"),
 		)
--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"log"
 )
 
+var version = "1"
+
 func main() {
 	hostname, _ := os.Hostname()
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, from %s! Our config is %q and our secret is %q\n",
+		fmt.Fprintf(w, "Hello, from %s! My version is %s, our config is %q and our secret is %q",
 			hostname,
+			version,
 			os.Getenv("CONFIG"),
 			os.Getenv("SECRET"),
 		)
